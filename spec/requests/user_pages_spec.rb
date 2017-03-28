3a38249b1da4ee26cceae2bad2f2e65e6bb6516f
RyotaHirano <hirano@tsumikiinc.com>
2017-03-28 12:19:05 +0900

    refactoring separate test-code from rogic

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,10 +39,7 @@ describe "UserPages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        valid_information
       end
 
       it "should create a user" do
@@ -55,7 +52,7 @@ describe "UserPages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_success_message('Welcome') }
       end
     end
   end
