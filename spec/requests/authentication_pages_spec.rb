d1f6359778b4b85ee6af3901d967259410c7baac
RyotaHirano <hirano@tsumikiinc.com>
2017-03-28 11:23:01 +0900

    Finish sign-in-out

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,7 +19,7 @@ describe "AuthenticationPages" do
       describe "after visiting another page" do
         before { click_link "Home" }
 
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message('Invalid') }
       end
     end
 
@@ -27,15 +27,19 @@ describe "AuthenticationPages" do
       let(:user) { FactoryGirl.create(:user) }
 
       before do
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        valid_signin(user)
       end
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+
+        it { should have_link('Sign in') }
+      end
     end
   end
 end
