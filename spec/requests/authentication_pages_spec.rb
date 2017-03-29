e7e19fa0b6dab8597902ca18e673ff256d58a22b
RyotaHirano <hirano@tsumikiinc.com>
2017-03-29 13:39:59 +0900

    add edit User Profile

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,13 +25,11 @@ describe "AuthenticationPages" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-
-      before do
-        valid_signin(user)
-      end
+      before { sign_in user }
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Setting', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
